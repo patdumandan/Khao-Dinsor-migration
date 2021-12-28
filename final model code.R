69bb42c7d73e5926c4462af80018fbd321e86fc7
Pat Dumandan <patriciakayeduma@u.boisestate.edu>
2021-12-27 22:15:20 -0500

    data cleaning, figure prep, and model codes

--- final model code.R
+++ final model code.R
@@ -1,84 +1,163 @@
-#KHAO DINSOR#
+#KHAO DINSOR####
 require(rstanarm)
 require(dplyr)
 ##within-season analysis
 #only use seaside counts
 
-kdata=read.csv(file.choose(), h=T)
-str(kdata)
+#raw data####
+kdata=read.csv("https://raw.githubusercontent.com/patdumandan/Khao-Dinsor-migration/master/BB-OHB-CS_daily.csv")
+kdata=kdata%>%select(-X)%>%
+  mutate(bph=Count/obs_effort)
 
-plot(kdata$Count~kdata$Barometric.Pressure)
+##add julian date####
+kdata$date <- as.Date(with(kdata, paste(Year, Month, Day,sep="-")), "%Y-%m-%d")
 
-#standardize data
-kdata$years = (kdata$Year - mean(kdata$Year))/(2 *sd(kdata$Year))
-kdata$temp = (kdata$temperature - mean(kdata$temperature))/(2 *sd(kdata$temperature))
-kdata$uwind = (kdata$u_wind - mean(kdata$u_wind))/(2 *sd(kdata$u_wind))
-kdata$vwind = (kdata$v_wind - mean(kdata$v_wind))/(2 *sd(kdata$v_wind))
-kdata$baro= (kdata$baro_pressure - mean(kdata$baro_pressure))/(2 *sd(kdata$baro_pressure))
-kdata$mon=as.character(kdata$Month)
-str(kdata)
+kdata$julian<- format(kdata$date, "%j")
 
+#add meteorological wd vector####
+kdata=kdata%>%
+  mutate(metwind= case_when(met_wind=="N"~ 0,  
+                            met_wind=="ESE"~ 112.5,
+                            met_wind=="NNE"~ 22.5,
+                            met_wind=="NE"~ 45,
+                            met_wind=="ENE"~ 67.5,
+                            met_wind=="E"~ 90,
+                            met_wind=="ESE"~ 112.5,
+                            met_wind=="SE"~135,
+                            met_wind=="SSE"~157.5,
+                            met_wind=="S"~180,
+                            met_wind=="SSW"~ 202.5,
+                            met_wind=="SW"~225,
+                            met_wind=="WSW"~247.5,
+                            met_wind=="W"~270,
+                            met_wind=="WNW"~292.5,
+                            met_wind=="NW"~315,
+                            met_wind=="NNW"~337.5))
+
+#u and v wind components####
+kdata$uwind=(-(kdata$wind_speed)* sin ((3.14/180)*kdata$metwind))
+kdata$vwind=(-(kdata$wind_speed)* cos ((3.14/180)*kdata$metwind))
+
+#standardize data####
+kdata$yearsd = (kdata$Year - mean(kdata$Year))/(2 *sd(kdata$Year))
+kdata$tempsd = (kdata$temperature - mean(kdata$temperature))/(2 *sd(kdata$temperature))
+kdata$uwindsd = (kdata$uwind - mean(kdata$uwind))/(2 *sd(kdata$uwind))
+kdata$vwindsd = (kdata$vwind - mean(kdata$vwind))/(2 *sd(kdata$vwind))
+kdata$barosd= (kdata$baro_pressure - mean(kdata$baro_pressure))/(2 *sd(kdata$baro_pressure))
+
+#Notes####
 #+ v wind is from the south
 # + u wind is from the west
 
-###subset for BB######
-bb=subset(kdata, Species=="BB")
-bb$logcount=log(bb$Count)
-boxplot(bb$Count~bb$met_wind, ylab=" number of individuals", xlab="wind direction")
+#add lag terms####
+kdata=kdata%>%mutate(lag_temp=lag(tempsd, order_by=date),
+                     lag_baro=lag(barosd, order_by = date),
+                     lag_uwind=lag(uwindsd, order_by = date),
+                     lag_vwind=lag(vwindsd, order_by=date),
+                     lag_count=lag(Count, order_by = date)
+)
 
-mod_bb=stan_glmer.nb(Count~years+temp+uwind+vwind+baro+(1|mon),
-                   data=bb)
-summary(mod_bb)
-length(which(bbpost$temp>0))/length(bbpost$temp)
+#MODELS####
+bb=kdata%>%filter(species=="Black Baza")
+cs=kdata%>%filter(species=="Chinese Sparrowhawk")
+ohb=kdata%>%filter(species=="Oriental Honey-buzzard")
 
-#subset for CS#####
-cs=subset(kdata, Species=="CS")
-plot(cs$Count~cs$met_wind)
-hist(bb$Count)
+mod_bb_lag=stan_glm(Count~yearsd+
+                      lag_temp+lag_uwind+lag_vwind+lag_baro,
+                    data=bb, family = neg_binomial_2, offset=obs_effort)
+mod_bb=stan_glm(Count~yearsd+tempsd+uwindsd+vwindsd+barosd,
+                data=bb, family = neg_binomial_2, offset=obs_effort)
 
-mod_cs=stan_glmer.nb(Count~years+temp+uwind+vwind+baro+(1|mon),
-                     data=cs)
-summary(mod_cs)
+mod_cs_lag=stan_glm(Count~yearsd+
+                      lag_temp+lag_uwind+lag_vwind+lag_baro,
+                    data=cs, family = neg_binomial_2, offset=obs_effort)
+mod_cs=stan_glm(Count~yearsd+tempsd+uwindsd+vwindsd+barosd,
+                data=cs, family = neg_binomial_2, offset=obs_effort)
 
-#subset for OHB####
+mod_ohb_lag=stan_glm(Count~yearsd+
+                       lag_temp+lag_uwind+lag_vwind+lag_baro,
+                     data=ohb, family = neg_binomial_2, offset=obs_effort)
+mod_ohb=stan_glm(Count~yearsd+tempsd+uwindsd+vwindsd+barosd,
+                 data=ohb, family = neg_binomial_2, offset=obs_effort)
 
-ohb=subset(kdata, Species=="OHB")
-plot(bb$Count~bb$barometric.pressure)
-hist(bb$Count)
+#plot model output:
+bbplot=as.matrix(mod_bb)
+bbp=mcmc_areas(bbplot, pars=c("tempsd","uwindsd", "vwindsd", "barosd",
+                          "yearsd"), prob=.80, prob_outer=0.95,
+           point_est = "median")+
+  ggplot2::scale_y_discrete(labels = c("temperature", "U wind", "V wind", "barometric pressure", "year"))+vline_0(size = 0.25, color = "darkgray", linetype = 2)+
+  ggtitle ("Black Baza")+ 
+  theme(plot.title=element_text(hjust=0,size=15),panel.border=element_blank(),
+        axis.line = element_line(colour = "black"),axis.title=element_text(size=12, 
+      color="black"),axis.text=element_text(size=12, hjust=0, color="black"),
+      axis.text.x=element_text(hjust=0.5, color="black"))+xlim(-12,5)
+bbp
 
-mod_ohb=stan_glmer.nb(Count~years+temp+uwind+vwind+baro+(1|mon),
-                     data=ohb)
-summary(mod_ohb)
-bbpost=as.data.frame(mod_bb)
+#plot model output:
+csplot=as.matrix(mod_cs)
+csp=mcmc_areas(csplot, pars=c("tempsd","uwindsd", "vwindsd", "barosd",
+                          "yearsd"), prob=.80, prob_outer=0.95,
+           point_est = "median")+
+  ggplot2::scale_y_discrete(labels = c("temperature", "U wind", "V wind", "barometric pressure", "year"))+vline_0(size = 0.25, color = "darkgray", linetype = 2)+
+  ggtitle ("Chinese Sparrowhawk")+ 
+  theme(plot.title=element_text(hjust=0,size=15),panel.border=element_blank(),
+        axis.line = element_line(colour = "black"),axis.title=element_text(size=12, 
+      color="black"),axis.text=element_text(size=12, hjust=0, color="black"),
+      axis.text.x=element_text(hjust=0.5, color="black"))+xlim(-12,5)
+csp
 
-
-#plots
-jpeg("wind2.jpeg",width = 7, height = 6, units = 'in', res = 300)
-plot(x, y) # Make plot
-dev.off()
-mcmc_areas(ohbp, pars=c("temp","uwind", "vwind", "baro",
-                                   "years"), prob=.80, prob_outer=0.95,
-                 point_est = "median")+
-  ggplot2::scale_y_discrete(labels = c("temperature", "U wind", "V wind", "barometric pressure", "years"))+vline_0(size = 0.25, color = "darkgray", linetype = 2)+
+ohbplot=as.matrix(mod_ohb)
+ohbp=mcmc_areas(ohbplot, pars=c("yearsd","tempsd","uwindsd", "vwindsd", "barosd"
+), prob=.80, prob_outer=0.95,
+point_est = "median")+ggplot2::scale_y_discrete(labels = c("year", "temperature", "U wind", "V wind", "barometric pressure"))+vline_0(size = 0.25, color = "darkgray", linetype = 2)+
   ggtitle ("Oriental Honey-buzzard")+ 
   theme(plot.title=element_text(hjust=0,size=15),panel.border=element_blank(),
-        axis.line = element_line(colour = "black"),axis.title=element_text(size=12, color="black"),axis.text=element_text(size=12, hjust=0, color="black"),axis.text.x=element_text(hjust=0.5, color="black"))+xlim(-6.5,5)
-bbplot
+        axis.line = element_line(colour = "black"),axis.title=element_text(size=12, color="black"),axis.text=element_text(size=12, hjust=0, color="black"),
+        axis.text.x=element_text(hjust=0.5, color="black"))+xlim(-12,5)
+ohbp
 
-ohbp=as.matrix(mod_ohb)
+plot_grid(bbp,
+          csp+theme(axis.text.y = element_blank(),
+                   axis.ticks.y = element_blank(),
+                   axis.title.y = element_blank()),
+          ohbp+theme(axis.text.y = element_blank(),
+                     axis.ticks.y = element_blank(),
+                     axis.title.y = element_blank()), 
+                     nrow=1, labels="auto", align="v")
 
-ggplot(kdata, aes(x = met_wind, y = Count)) +
-  geom_boxplot(notch = FALSE, aes(ymin = y0, lower = y25, middle = y50, upper = y75, ymax = y100)) +
-  facet_grid( Species ~ . ) +
-  labs(x = 'wind direction', y = 'number of individuals') +
-  theme_bw() +scale_y_log10()+
-  theme(
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank()
-  )
-graph
+#P(positive/negative effects)####
 
-kdata2=kdata%>% 
-       mutate(species_id= case_when(Species=="Black Baza" ~ "BB", Species=="Chinese Sparrowhawk" ~ "CS", Species== "Oriental Honey-Buzzard" ~"OHB" ) )
+bbpos=as.data.frame(mod_bb)
+length(which(bbpos$uwindsd<0))/length(bbpos$uwindsd)
+length(which(bbpos$vwindsd<0))/length(bbpos$vwindsd)
+ength(which(bbpos$tempsd<0))/length(bbpos$tempsd)
+length(which(bbpos$barosd<0))/length(bbpos$barosd)
 
-str(kdata2)
+cspos=as.data.frame(mod_cs)
+length(which(cspos$uwindsd>0))/length(cspos$uwindsd)
+length(which(cspos$vwindsd>0))/length(cspos$vwindsd)
+length(which(cspos$tempsd>0))/length(cspos$tempsd)
+length(which(cspos$barosd<0))/length(cspos$barosd)
+
+ohbpos=as.data.frame(mod_ohb)
+length(which(ohbpos$uwindsd>0))/length(ohbpos$uwindsd)
+length(which(ohbpos$vwindsd>0))/length(ohbpos$vwindsd)
+length(which(ohbpos$tempsd>0))/length(ohbpos$tempsd)
+length(which(ohbpos$barosd<0))/length(ohbpos$barosd)
+
+#seasonal timing plots####
+
+
+bb_1=plot(bb$Count~bb$julian, ylab="number of individuals", xlab="Julian date", pch=19, main="Black Baza")
+cs_1=plot(cs$Count~cs$julian, ylab="number of individuals", xlab="Julian date", pch=19, main="Chinese Sparrowhawk")
+ohb_1=plot(ohb$Count~ohb$julian, ylab="number of individuals", xlab="Julian date", pch=19, main="Oriental Honey-buzzard")
+
+#weather plots####
+grid.arrange(temp_plot, bp_plot)
+plot(kdata$temperature~kdata$julian, 
+               ylab="temperature(Celsius)", xlab="Julian date", 
+               main="temperature", pch=19)
+
+plot(kdata$baro_pressure~kdata$julian, ylab="barometric pressure (hPa)", xlab="Julian date", main="barometric pressure", pch=19)
+plot(kdata$u_wind~kdata$julian, ylab="U wind (W-E)", xlab="Julian date", main="U wind", pch=19)
+plot(kdata$v_wind~kdata$julian, ylab="V wind (N-S)", xlab="Julian date", main="V wind", pch=19)
